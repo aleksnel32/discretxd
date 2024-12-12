c931444514f4f80a6e1c8cfe414770290a25cc40
aleksnel32 <83445256+aleksnel32@users.noreply.github.com>
2024-12-12 19:01:27 +0500

    Update task1.py
    гыы низя юзать мин макс

--- variant4/task1.py
+++ variant4/task1.py
@@ -1,7 +1,16 @@
 #М не требуется, вводить сразу массив
 #вводить массив через пробел
+
+
+def minv(list):
+    result = 9999999
+    for item in list:
+        if result < item:
+            result = item
+    return item
+
 B = list(map(int, input().split()))
-imin = B.index(min(B))
+imin = B.index(minv(B))
 
 B.pop(imin)
 print(B)
