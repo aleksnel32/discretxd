3d38f4c60d7dc6e3a8268dae7fc2cc3d42673a7e
aleksnel32 <83445256+aleksnel32@users.noreply.github.com>
2024-12-12 19:00:43 +0500

    Update task1.py
    гыыы низя юзать мин макс

--- variant3/task1.py
+++ variant3/task1.py
@@ -2,8 +2,23 @@
 #через пробел элементы массива вводить
 B = list(map(int, input().split()))
 
-imin = B.index(min(B))
-imax = B.index(max(B))
+
+def maxv(list):
+    result = 0
+    for item in list:
+        if result > item:
+            result = item
+    return item
+
+def minv(list):
+    result = 9999999
+    for item in list:
+        if result < item:
+            result = item
+    return item
+
+imin = B.index(minv(B))
+imax = B.index(maxv(B))
 
 B[imin], B[imax] = B[imax], B[imin]
 
