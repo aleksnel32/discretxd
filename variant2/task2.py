d055216eda9d98c9e53e175b3a4070fba531c80a
aleksnel32 <83445256+aleksnel32@users.noreply.github.com>
2024-12-14 01:52:19 +0500

    Update task2.py

--- variant2/task2.py
+++ variant2/task2.py
@@ -3,7 +3,7 @@ w = []
 
 for i in range(5):
     r = list(map(int, input().split()))
-    if i % 2 == 0: # вот тут прикол считает ли она четным 0 xd) если считает то все верно, а если нет нужно добавить условие "and i != 0"
+    if (i+1) % 2 == 0:
         for i, element in enumerate(r):
             if abs(element) < 45625:
                 r[i] = 0
